62ce1b68587b584fea381d427cbadfbaac1ae717
Diego Diomir <aries-d2github@hotmail.com>
2025-07-16 11:12:15 -0400

    feat: Primera configuracion

---
+++ api-sicopi/src/main/java/com/sicopi/SicopiApplication.java
@@ -0,0 +1,11 @@
+package com.sicopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicopiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SicopiApplication.class, args);
+    }
+}
