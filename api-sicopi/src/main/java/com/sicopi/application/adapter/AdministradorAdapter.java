1fd35ffaddf5ae0595b65540355cc3b3a48597f9
Diego Diomir <aries-d2github@hotmail.com>
2025-07-20 20:22:55 -0400

    feat: refactor: Implementamos el primero adaptador de Administrador mas las persistencia out

---
+++ api-sicopi/src/main/java/com/sicopi/application/adapter/AdministradorAdapter.java
@@ -0,0 +1,14 @@
+package com.sicopi.application.adapter;
+
+import com.sicopi.application.port.out.persistence.AdministracionAbs;
+
+public class AdministradorAdapter {
+
+    private final AdministracionAbs administracionAbs;
+
+    public AdministradorAdapter(AdministracionAbs administracionAbs) {
+        this.administracionAbs = administracionAbs;
+    }
+
+
+}
--- api-sicopi/src/main/java/com/sicopi/application/port/in/AdministradorService.java
+++ api-sicopi/src/main/java/com/sicopi/application/port/in/AdministradorService.java
@@ -71,4 +71,9 @@ public interface AdministradorService {
 
 
 //    PaginableOut<UsuarioUnidad> listaDeUsuarios(PaginableIn paginableIn);
+
+
+    //Metodo que no necesita de PUERTOS salientes por ser un metodo de negocio
+    // tipo calular algo cosas asi
+    void calcularImpuesto();
 }
